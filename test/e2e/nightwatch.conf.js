124fc6693ffaba139d8f61880e92e437a1ba3a79
João Victor <joaovpbarbosa@gmail.com>
2017-11-26 01:13:39 -0300

    change nightwatch

--- test/e2e/nightwatch.conf.js
+++ test/e2e/nightwatch.conf.js
@@ -6,32 +6,29 @@ module.exports = {
   src_folders: ['test/e2e/specs'],
   output_folder: 'test/e2e/reports',
   custom_assertions_path: ['test/e2e/custom-assertions'],
-
-  test_settings: {
-    default: {
+  "selenium": {
+    "start_process": false
+  },
+  "test_settings": {
+    "default": {
+      "selenium_host": "127.0.0.1",
+      "selenium_port": "9515",
+      "screenshots": {
+        "enabled": false,
+      },
       "desiredCapabilities": {
-        "browserName": "phantomjs",
+        "browserName": "chrome",
         "javascriptEnabled" : true,
         "acceptSslCerts" : true,
-        "phantomjs.binary.path" : "phantomjs",
-        "phantomjs.cli.args" : ['--ignore-ssl-errors=true']
-      }
-    },
-
-    chrome: {
-      desiredCapabilities: {
-        browserName: 'chrome',
-        javascriptEnabled: true,
-        acceptSslCerts: true
-      }
-    },
-
-    firefox: {
-      desiredCapabilities: {
-        browserName: 'firefox',
-        javascriptEnabled: true,
-        acceptSslCerts: true
-      }
+        "chromeOptions" : {
+          "args": [
+            "--no-sandbox",
+            "start-fullscreen",
+            "window-size=1280,800"
+          ]
+        }
+      },
+      "launch_url": "http://app"
     }
   }
 }
